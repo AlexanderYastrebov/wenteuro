1e7d9ad67ff5452bbaddf5742c58d5642028b414
Alexander Yastrebov <yastrebov.alex@gmail.com>
2016-09-30 17:17:45 +0200

    Implemented main class

--- src/main/java/com/github/ayastrebov/wenteuro/Main.java
+++ src/main/java/com/github/ayastrebov/wenteuro/Main.java
@@ -1,8 +1,36 @@
 package com.github.ayastrebov.wenteuro;
 
+import com.github.ayastrebov.wenteuro.api.PositionApiGateway;
+import com.github.ayastrebov.wenteuro.output.PositionCsvWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import lombok.Value;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    @Value
+    private static class Arguments {
+
+        String text;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Arguments arguments = parseArgs(args);
+
+        try (OutputStream out = System.out) {
+            new Processor(new PositionApiGateway(), new PositionCsvWriter(out)).process(arguments.getText());
+        }
+    }
+
+    private static Arguments parseArgs(String[] args) {
+        if (args.length != 1) {
+            System.err.println(""
+                + "Arguments: <text>\n"
+                + "\n"
+                + "    text      position suggestion API input text\n"
+            );
+            System.exit(1);
+        }
+        return new Arguments(args[0]);
     }
 }
